f15d09cd5b72b0d37be647554ac7d781682cf99e
arjundangi01 <arjundangi907473@gmail.com>
2023-11-04 16:44:49 +0530

    feat : implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { ThemeContext } from './context/test.context';
+import React from "react";
+import logo from "./logo.svg";
+import  { ThemeContext } from "./context/test.context";
+import ThemeContextProvider, { ThemeContext as test  } from "./context/test.context";
+import Card from "./components/card";
 // import {}
 function App() {
-  let value = React.useContext(ThemeContext)
-  console.log(value)
+  let value = React.useContext(ThemeContext);
+  console.log(value);
   return (
     <div className="App">
-      
+      <ThemeContextProvider>
+        <Card />
+      </ThemeContextProvider>
     </div>
-  );
+  ); 
 }
 
 export default App;
